bb68c0a4a668653765df4d18f515d842b9e2ff8f
gg17117144 <106013193+gg17117144@users.noreply.github.com>
2023-02-11 03:40:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def generate_text(prompt):
     message = completions.choices[0].text
     return message
 
-st.title("告白文生成器")
+st.title("告白生成器")
 
 sender_interests = st.text_input("您的興趣是？")
 sender_abilities = st.text_input("您的能力是？")
@@ -30,7 +30,7 @@ prompt = (f"告白。我的興趣是{sender_interests}，我的能力是{sender_
          f"我喜歡對方是因為{sender_love_features}。對方的興趣是{recipient_interests}，"
          f"我想對你說{recipient_features}！我喜歡你！請你和我交往！")
 
-if st.button("生成告白文"):
+if st.button("生成告白"):
     love_letter = generate_text(prompt)
-    st.write("生成的告白文：", love_letter)
+    st.write("生成的告白：", love_letter)
 
